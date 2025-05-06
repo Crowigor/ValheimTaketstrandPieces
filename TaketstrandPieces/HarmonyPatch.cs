347e28b7c934083cdcfb8aee795ad70899926faf
Igor Berdichevskiy <iberdichevskiy@septdir.com>
2025-05-06 23:40:50 +0300

    1.0.1 update

--- TaketstrandPieces/HarmonyPatch.cs
+++ TaketstrandPieces/HarmonyPatch.cs
@@ -49,42 +49,36 @@ public static class ContainerGetHoverTextPatch
 }
 
 [HarmonyPatch(typeof(Player), nameof(Player.UseHotbarItem))]
-public static class PreventUseHotbarItemNearSignChest
+public static class InterceptUseHotbarItemOnNamedChest
 {
     static bool Prefix(Player __instance, int index)
     {
-        var cam = Camera.main;
-        if (cam == null)
+        var hoverObject = __instance.GetHoverObject();
+        if (hoverObject == null)
             return true;
-
-        if (!Physics.Raycast(cam.transform.position, cam.transform.forward, out var hit, 7f))
-            return true;
-
-        var piece = hit.collider.GetComponentInParent<Piece>();
+        
+        var piece = hoverObject.GetComponentInParent<Piece>();
         if (piece == null || piece.m_name != "$tkd_named_chest")
             return true;
-
+        
         var sign = piece.GetComponent<Sign>();
         if (sign == null)
             return true;
-
+        
         var item = __instance.GetInventory()?.GetItemAt(index - 1, 0);
         if (item == null)
             return true;
 
-        var prefabName = item.m_dropPrefab != null ? item.m_dropPrefab.name : item.m_shared.m_name;
+        var prefabName = item.m_dropPrefab ? item.m_dropPrefab.name : item.m_shared.m_name;
+        
         var suffix = "#_mode_all";
         if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
-        {
             suffix = "#_mode_text";
-        }
         else if (Input.GetKey(KeyCode.LeftAlt) || Input.GetKey(KeyCode.RightAlt))
-        {
             suffix = "#_mode_icon";
-        }
-
+        
         sign.SetText("#" + prefabName + suffix);
-
+        
         return false;
     }
 }
